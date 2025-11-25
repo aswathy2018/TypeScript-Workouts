e7417f22184ac0c6fecbc747c4dd49065af71630
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-11-25 09:44:36 +0530

    Added

---
+++ paramDestructure.js
@@ -0,0 +1,5 @@
+function dest(_a) {
+    var name = _a.name, age = _a.age;
+    console.log("Name: ".concat(name, ", Age: ").concat(age));
+}
+dest({ name: "Aswathy", age: 23 });
---
+++ paramDestructure.ts
@@ -0,0 +1,5 @@
+function dest({name, age}: {name: string, age: number}):void{
+    console.log(`Name: ${name}, Age: ${age}`)
+}
+
+dest({name: "Aswathy", age: 23})
